255b27da77ed012fdc464371301e6855c042adea
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-15 09:32:11 +0100

    exporting function getBrowser so it is visible from the outside.

--- detect-browser.js
+++ detect-browser.js
@@ -1,29 +1,31 @@
 // @ts-nocheck
 'use strict';
-// detect-browser.js v1.0.0
-// Get Browser Data
+/*
+ detect-browser.js v1.0.0
+ Get Browser Data
 
-// MIT License
+ MIT License
 
-// Copyright (c) 2018 Ahmad Raza
+ Copyright (c) 2018 Ahmad Raza
 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
 
 
 function isMobile() {
@@ -59,7 +61,7 @@ function getBrowserName() {
 	if (!(document.documentMode) && window.StyleMedia) {
 		return 'Microsoft Edge';
 	}
-	
+
 	// Chrome
 	if (window.chrome) {
 		return 'Chrome';
@@ -156,15 +158,6 @@ function getBrowser() {
 	};
 }
 
-
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', 'http://ip-api.com/json');
-// xhr.onreadystatechange = function () {
-// 	if (xhr.readyState == 4) {
-// 		if (xhr.status == 200) {
-// 			var IPdata = xhr.responseText;
-// 			jsonResponse = JSON.parse(IPdata);
-// 		}
-// 	}
-// };
-// xhr.send(null);
+module.exports = {
+	getBrowser: getBrowser,
+};
